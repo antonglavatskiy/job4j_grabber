edccd6a39a61c099be74331377a1e83375130212
antonglavatskiy <glavazky@yandex.ru>
2022-07-25 10:41:29 +0500

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -7,6 +7,9 @@ import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 import java.util.Properties;
 
@@ -33,6 +36,25 @@ public class Grabber implements Grab {
 
     public Store store() {
         return new PsqlStore(config);
+    }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(config.getProperty("port")))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store().getAll()) {
+                            out.write(post.toString().getBytes());
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     @Override
@@ -76,5 +98,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grabber.scheduler();
         Store store = grabber.store();
         grabber.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+        grabber.web(store);
     }
 }
